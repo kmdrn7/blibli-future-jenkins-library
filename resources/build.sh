c3bb902aa124fb770f3bb22e234fffa003d6d0db
Andika Ahmad Ramadhan <ASpendaka@gmail.com>
2021-02-13 22:22:49 +0700

    update build.sh

--- resources/build.sh
+++ resources/build.sh
@@ -1,6 +1,6 @@
-echo "============================================================"
+echo "==============================="
 echo "+ Running as $(whoami) user."
-echo "============================================================"
+echo "==============================="
 echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USER} --password-stdin
 docker build .
   --tag ${CONTAINER_REGISTRY}/${CONTAINER_IMAGE}:${CONTAINER_VERSION}
