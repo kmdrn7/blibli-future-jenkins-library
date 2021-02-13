5fe7c5946dd83f1df04b5b7f7b32064614fdee90
Andika Ahmad Ramadhan <ASpendaka@gmail.com>
2021-02-13 22:38:39 +0700

    fix newline command

--- resources/build.sh
+++ resources/build.sh
@@ -1,8 +1,6 @@
-echo "==============================="
-echo "+ Running as $(whoami) user."
-echo "==============================="
+echo "Running as $(whoami) user."
 echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USER} --password-stdin
-docker build .
-  --tag ${CONTAINER_REGISTRY}/${CONTAINER_IMAGE}:${CONTAINER_VERSION}
+docker build . \
+  --tag ${CONTAINER_REGISTRY}/${CONTAINER_IMAGE}:${CONTAINER_VERSION} \
   --tag ${CONTAINER_REGISTRY}/${CONTAINER_IMAGE}:latest
 docker push ${CONTAINER_REGISTRY}/${CONTAINER_IMAGE}:${CONTAINER_VERSION}
