43e39917cf1a7fcc46c3938afdb589d23f14c9db
Andika Ahmad Ramadhan <ASpendaka@gmail.com>
2021-02-14 01:40:11 +0700

    Fix helm repo host

--- resources/deliver.sh
+++ resources/deliver.sh
@@ -1,3 +1,7 @@
+kubectl config set-cluster k8s --server="${KUBE_ENDPOINT}" \
+&& kubectl config set-credentials jenkins --token="${KUBE_TOKEN}" \
+&& kubectl config set-context default --cluster=k8s --user=jenkins \
+&& kubectl config use-context default
 helm upgrade \
-  --install ${CONTAINER_IMAGE} http://chartmuseum.mykloud.lokal:8088/charts/${CONTAINER_IMAGE}-${CONTAINER_VERSION}.tgz \
+  --install ${CONTAINER_IMAGE} http://10.174.0.12:8088/charts/${CONTAINER_IMAGE}-${CONTAINER_VERSION}.tgz \
   --set image.name=${CONTAINER_REGISTRY}/${CONTAINER_IMAGE}
--- resources/packageAndShip.sh
+++ resources/packageAndShip.sh
@@ -3,4 +3,5 @@ kubectl config set-cluster k8s --server="${KUBE_ENDPOINT}" \
 && kubectl config set-context default --cluster=k8s --user=jenkins \
 && kubectl config use-context default
 helm package .helm
+http -f DELETE http://10.174.0.12:8088/api/charts/${CONTAINER_IMAGE}/${CONTAINER_VERSION}
 http -f POST http://10.174.0.12:8088/api/charts chart@${CONTAINER_IMAGE}-${CONTAINER_VERSION}.tgz --ignore-stdin
