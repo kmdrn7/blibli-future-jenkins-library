ce88f7b813d39b80a68d9f9e29db77c9d68f3373
Andika Ahmad Ramadhan <ASpendaka@gmail.com>
2021-02-14 00:35:18 +0700

    add script packageAndShip.sh

--- resources/packageAndShip.sh
+++ resources/packageAndShip.sh
@@ -1,4 +1,6 @@
 kubectl config set-cluster k8s --server="${KUBE_ENDPOINT}" \
-  && kubectl config set-credentials jenkins --token="${KUBE_TOKEN}" \
-  && kubectl config set-context default --cluster=k8s --user=jenkins \
-  && kubectl config use-context default
+&& kubectl config set-credentials jenkins --token="${KUBE_TOKEN}" \
+&& kubectl config set-context default --cluster=k8s --user=jenkins \
+&& kubectl config use-context default
+helm package .helm
+http -f POST http://10.174.0.12:8088/api/charts chart@${CONTAINER_IMAGE}-${CONTAINER_VERSION}.tgz
