18db242e6f814b2047f6274266c2d60560c414e3
Andika Ahmad Ramadhan <ASpendaka@gmail.com>
2021-02-17 18:01:44 +0700

    Update full pipeline

--- vars/deployPipeline.groovy
+++ vars/deployPipeline.groovy
@@ -34,48 +34,42 @@ def call(Map params){
                     }
                 }
             }
-            // stage('Package Application with HELM') {
-            //     agent {
-            //         docker {
-            //             image 'kmdr7/helm-kubectl:latest'
-            //         }
-            //     }
-            //     steps {
-            //         withEnv([
-            //             'CONTAINER_REGISTRY='+params.containerRegistry,
-            //             'CONTAINER_IMAGE='+params.containerImage,
-            //             'CONTAINER_VERSION='+params.containerVersion,
-            //         ]){
-            //             sh(packageAndShip)
-            //         }
-            //     }
-            // }
-            // stage('Deploy Application to Kubernetes') {
-            //     agent {
-            //         docker {
-            //             image 'kmdr7/helm-kubectl:latest'
-            //         }
-            //     }
-            //     steps {
-            //         withCredentials([
-            //             usernamePassword(
-            //                 credentialsId: 'cred-kubernetes',
-            //                 usernameVariable: 'KUBE_ENDPOINT',
-            //                 passwordVariable: 'KUBE_TOKEN'
-            //             )
-            //         ]) {
-            //             withEnv([
-            //                 'CONTAINER_REGISTRY='+params.containerRegistry,
-            //                 'CONTAINER_IMAGE='+params.containerImage,
-            //                 'CONTAINER_VERSION='+params.containerVersion,
-            //                 'NAMESPACE='+params.namespace,
-            //             ]){
-            //                 sh(prepareDeliver)
-            //                 sh(deliver)
-            //             }
-            //         }
-            //     }
-            // }
+            stage('Package Application with HELM') {
+                steps {
+                    container('helm-kubectl') {
+                        withEnv([
+                            'CONTAINER_REGISTRY='+params.containerRegistry,
+                            'CONTAINER_IMAGE='+params.containerImage,
+                            'CONTAINER_VERSION='+params.containerVersion,
+                        ]){
+                            sh(packageAndShip)
+                        }
+                    }
+                }
+            }
+            stage('Deploy Application to Kubernetes') {
+                steps {
+                    container('helm-kubectl') {
+                        withCredentials([
+                            usernamePassword(
+                                credentialsId: 'cred-kubernetes',
+                                usernameVariable: 'KUBE_ENDPOINT',
+                                passwordVariable: 'KUBE_TOKEN'
+                            )
+                        ]) {
+                            withEnv([
+                                'CONTAINER_REGISTRY='+params.containerRegistry,
+                                'CONTAINER_IMAGE='+params.containerImage,
+                                'CONTAINER_VERSION='+params.containerVersion,
+                                'NAMESPACE='+params.namespace,
+                            ]){
+                                sh(prepareDeliver)
+                                sh(deliver)
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
