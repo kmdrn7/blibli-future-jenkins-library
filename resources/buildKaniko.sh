a38f6a66f2134975c35a2d129ad9a134e7097ea5
Andika Ahmad Ramadhan <ASpendaka@gmail.com>
2021-02-17 17:26:47 +0700

    remove kaniko, update pod resource request

--- resources/buildKaniko.sh
+++
@@ -1,7 +0,0 @@
-echo "Running as $(whoami) user."
-echo '{"auths":{"https://index.docker.io/v1/":{"auth":"'$(echo -n ${DOCKER_USER}:${DOCKER_PASSWORD} | base64)'"}}}' > /kaniko/.docker/config.json
-/kaniko/executor \
-  --dockerfile `pwd`/Dockerfile \
-  --context `pwd` \
-  --destination=${CONTAINER_REGISTRY}/${CONTAINER_IMAGE}:${CONTAINER_VERSION}
-  --destination=${CONTAINER_REGISTRY}/${CONTAINER_IMAGE}:latest
