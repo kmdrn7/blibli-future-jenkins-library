5c77ff6ebb543fa9ab9a223edef67af495660062
Andika Ahmad Ramadhan <ASpendaka@gmail.com>
2021-02-17 18:11:55 +0700

    Update helm repository url

--- resources/deliver.sh
+++ resources/deliver.sh
@@ -1,3 +1,3 @@
 helm upgrade \
-  --install ${CONTAINER_IMAGE} http://10.174.0.12:8088/charts/${CONTAINER_IMAGE}-${CONTAINER_VERSION}.tgz \
+  --install ${CONTAINER_IMAGE} http://helm.andikaahmad.com:8088/charts/${CONTAINER_IMAGE}-${CONTAINER_VERSION}.tgz \
   --set image.name=${CONTAINER_REGISTRY}/${CONTAINER_IMAGE}
--- resources/packageAndShip.sh
+++ resources/packageAndShip.sh
@@ -1,3 +1,3 @@
 helm package .helm
-http -f DELETE http://10.174.0.12:8088/api/charts/${CONTAINER_IMAGE}/${CONTAINER_VERSION}
-http -f POST http://10.174.0.12:8088/api/charts chart@${CONTAINER_IMAGE}-${CONTAINER_VERSION}.tgz --ignore-stdin
+http -f DELETE http://helm.andikaahmad.com:8088/api/charts/${CONTAINER_IMAGE}/${CONTAINER_VERSION}
+http -f POST http://helm.andikaahmad.com:8088/api/charts chart@${CONTAINER_IMAGE}-${CONTAINER_VERSION}.tgz --ignore-stdin
